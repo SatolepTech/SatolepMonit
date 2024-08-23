5c3ac67b92b46aeac8fa9f0f7f68761ada806527
Bruno Ferraz Silveira <bfssfb@outlook.com.br>
2024-08-23 17:42:13 -0300

    chore: add PM2 config

---
+++ pm2-process.config.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+import { configDotenv } from 'dotenv'
+
+configDotenv()
+
+const name = process.env.PM2_NAME ?? 'satolep-monit'
+const instances = process.env.PM2_INSTANCES ?? 1
+const listenTimeout = process.env.PM2_LISTEN_TIMEOUT ?? 10000
+const restartDelay = process.env.PM2_RESTART_DELAY ?? 10000
+
+export const apps = [
+  {
+    name,
+    script: 'dist/main.js',
+    instances,
+    interpreter: '/bin/bash',
+    exec_mode: 'fork',
+    'listen-timeout': listenTimeout,
+    restart_delay: Number(restartDelay),
+    exp_backoff_restart_delay: 100,
+    cwd: './'
+  }
+]
