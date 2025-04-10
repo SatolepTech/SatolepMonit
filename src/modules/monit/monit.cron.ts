1217d5ab8200783c3259d1dd203dc2eac97e7dbc
Bruno Ferraz Silveira <bfssfb@outlook.com.br>
2025-04-09 23:18:46 -0300

    feat: add monit cron

--- src/modules/monit/monit.cron.ts
+++ src/modules/monit/monit.cron.ts
@@ -1,84 +1,147 @@
+import { Injectable } from '@nestjs/common'
 import { Cron, CronExpression } from '@nestjs/schedule'
 
 import { DiscordMessageBuilder } from '../_infra/discord-webhook/discord-message.builder'
+import { ServerService } from '../server/server.service'
 
+import { formatDatetime } from 'src/common/date'
 import { logError } from 'src/common/log-error'
+import { client } from 'src/config/client'
 import { env } from 'src/config/env'
 import { DiscordWebhookPayloadType } from 'src/domain/discord'
 import { MonitResponse } from 'src/domain/monit'
+import {
+  HealthResponse,
+  ServerDTO,
+  ServersStatusDTO
+} from 'src/domain/server'
 
+@Injectable()
 export class MonitCron {
+  constructor(private readonly serverService: ServerService) {}
+
+  discord = new DiscordMessageBuilder()
+
+  icon: Record<DiscordWebhookPayloadType, string> = {
+    Alert: ':warning:',
+    Danger: ':x:',
+    Disable: ':white_check_mark:',
+    Info: ':white_check_mark:',
+    Success: ':white_check_mark:'
+  }
+
   @Cron(CronExpression.EVERY_30_MINUTES)
   async serverStatusCron() {
     const envData = await env()
-    if (!envData.monitURLs) return
 
-    let type = DiscordWebhookPayloadType.Info
+    const result = await this.serverService.getServers()
+    if (!result || !result.success) return
 
-    const [dangerIcon, okIcon, warningIcon] = [
-      ':x:',
-      ':white_check_mark:',
-      ':warning:'
-    ]
-
+    const servers = result.data
     const dangerValue = envData.alertValue.danger
     const warningValue = envData.alertValue.warning
 
-    const dangers: string[] = []
-    const notGetData: string[] = []
-    const values: string[] = []
+    const { dangers, notGetData, warning } =
+      await this.getServersStatuses({
+        dangerValue,
+        servers,
+        warningValue
+      })
 
-    const urls = envData.monitURLs.split(';')
+    const hasAlert =
+      warning.length > 0 ||
+      dangers.length > 0 ||
+      notGetData.length > 0
+    if (!hasAlert) return
 
-    for (let index = 0; index < urls.length; index++) {
-      let icon = okIcon
-      const url = urls[index]
+    await this.discord.sendMessage({
+      field: {
+        name: 'Status',
+        value: formatDatetime(new Date())
+      },
+      title: envData.discord.title || 'Servidores',
+      type: DiscordWebhookPayloadType.Info
+    })
+
+    await this.sendServersWarningMessage({
+      panicErrorsUsers: envData.discord.panicErrorsUsers,
+      warning
+    })
+
+    await this.sendServersDangersMessage({
+      dangers,
+      panicErrorsUsers: envData.discord.panicErrorsUsers
+    })
+
+    await this.sendServersNotGetDataMessage({
+      notGetData,
+      panicErrorsUsers: envData.discord.panicErrorsUsers
+    })
+  }
+
+  private async getServersStatuses({
+    dangerValue,
+    warningValue,
+    servers
+  }: {
+    dangerValue: number
+    servers: ServerDTO[]
+    warningValue: number
+  }) {
+    const dangersServers: ServersStatusDTO[] = []
+    const notGetDataServers: ServersStatusDTO[] = []
+    const okServers: ServersStatusDTO[] = []
+    const warningServers: ServersStatusDTO[] = []
+
+    for (let index = 0; index < servers.length; index++) {
+      const server = servers[index]
+
+      const route = server.Route
+      if (!route) continue
+
+      const token = server.token
+      const url = `${server.url}/${route.monit}`
 
       try {
-        const response = await fetch(url)
-        const data: MonitResponse = await response.json()
+        const resultMonit: MonitResponse = await client({
+          searchParams: [{ token }],
+          url
+        })
 
-        if (!data || !data.success) {
-          notGetData.push(`* ${url}!`)
+        if (!resultMonit || !resultMonit.success) {
+          notGetDataServers.push({ server })
           continue
         }
 
         const {
-          serverStatus: { server, status }
-        } = data
+          serverStatus: { status }
+        } = resultMonit
 
         const cpu = status.cpu
         const disk = status.disk
         const ram = status.ram
 
         if (
-          cpu > warningValue ||
-          disk > warningValue ||
-          ram > warningValue
-        ) {
-          icon = warningIcon
-          type = DiscordWebhookPayloadType.Alert
-        }
-
-        if (
           cpu > dangerValue ||
           disk > dangerValue ||
           ram > dangerValue
         ) {
-          dangers.push(
-            `* ${server.name}: ${server.ip}, ${server.slug}`
-          )
-          icon = dangerIcon
-          type = DiscordWebhookPayloadType.Danger
+          dangersServers.push({ server, status })
+          continue
         }
 
-        values.push(
-          `* ${
-            icon
-          } ${server.name} - CPU: ${status.cpu}% Disco: ${status.disk}% RAM: ${status.ram}%`
-        )
+        if (
+          cpu > warningValue ||
+          disk > warningValue ||
+          ram > warningValue
+        ) {
+          warningServers.push({ server, status })
+          continue
+        }
+
+        okServers.push({ server, status })
       } catch (error) {
-        notGetData.push(`* ${url}`)
+        notGetDataServers.push({ server })
         logError({
           archive: 'src/modules/monit/monit.cron.ts',
           error,
@@ -87,66 +150,130 @@ export class MonitCron {
       }
     }
 
-    if (dangers.length > 0) {
-      dangers.unshift(
-        `Atenção ${envData.discord.panicErrorsUsers}, os seguintes servidores excederam a margem de uso recomendada:`
-      )
+    return {
+      dangers: dangersServers,
+      notGetData: notGetDataServers,
+      ok: okServers,
+      warning: warningServers
     }
+  }
 
-    if (notGetData.length > 0) {
-      notGetData.unshift(
-        `Atenção ${envData.discord.panicErrorsUsers}, não foi possível recuperar os dados das seguintes URLs:`
-      )
-    }
-
-    const dangersMessage =
-      dangers.length > 0 ? '\n\n' + dangers.join('\n') : ''
-
-    const notGetDataMessage =
-      notGetData.length > 0 ? '\n\n' + notGetData.join('\n') : ''
-
-    const discord = new DiscordMessageBuilder()
-
-    const splitArray = []
+  private async sendDiscordMessages({
+    serversStatus,
+    title,
+    type
+  }: {
+    serversStatus: ServersStatusDTO[]
+    title: string
+    type: DiscordWebhookPayloadType
+  }) {
+    const splitArray: ServersStatusDTO[][] = []
     const subarrayLength = 5
 
-    for (let i = 0; i < values.length; i += subarrayLength) {
-      const subarray = values.slice(i, i + subarrayLength)
+    for (let i = 0; i < serversStatus.length; i += subarrayLength) {
+      const subarray = serversStatus.slice(i, i + subarrayLength)
       splitArray.push(subarray)
     }
 
     for (let index = 0; index < splitArray.length; index++) {
-      const array = splitArray[index]
-      await discord.sendMessage({
-        field: {
-          name: `Status - Parte ${index + 1} / ${splitArray.length}:`,
-          value: array.join('\n')
-        },
-        title: envData.discord.title,
-        type
-      })
-    }
+      const serversStatus = splitArray[index]
 
-    if (dangersMessage) {
-      await discord.sendMessage({
-        field: {
-          name: 'Uso alto de recursos:',
-          value: dangersMessage
-        },
-        title: envData.discord.title,
-        type
-      })
-    }
+      const serversStatusMessage = serversStatus.map(
+        serverStatus =>
+          `* ${this.icon[DiscordWebhookPayloadType[type]]} ${serverStatus.server.name} (${serverStatus.server.ip}): ${
+            serverStatus.status
+              ? `[CPU - ${serverStatus.status.cpu}%] [Disco - ${serverStatus.status.disk}%] [RAM - ${serverStatus.status.ram}%]`
+              : 'Não foi possui pegar o status'
+          }`
+      )
 
-    if (notGetDataMessage) {
-      discord.sendMessage({
+      await this.discord.sendMessage({
         field: {
-          name: 'Não foi possível recuperar informações:',
-          value: notGetDataMessage
+          name: `Parte ${index + 1} / ${splitArray.length}:`,
+          value: serversStatusMessage.join('\n')
         },
-        title: envData.discord.title,
+        title,
         type
       })
     }
+  }
+
+  private async sendServersDangersMessage({
+    dangers,
+    panicErrorsUsers
+  }: {
+    dangers: ServersStatusDTO[]
+    panicErrorsUsers: string
+  }) {
+    if (dangers.length > 0) {
+      await this.discord.sendMessage({
+        field: {
+          name: `Atenção ${panicErrorsUsers}`,
+          value:
+            'Os seguintes servidores excederam a margem de uso recomendada'
+        },
+        title: 'Urgência!',
+        type: DiscordWebhookPayloadType.Danger
+      })
+
+      await this.sendDiscordMessages({
+        serversStatus: dangers,
+        title: 'Urgência!',
+        type: DiscordWebhookPayloadType.Danger
+      })
+    }
+  }
+
+  private async sendServersNotGetDataMessage({
+    notGetData,
+    panicErrorsUsers
+  }: {
+    notGetData: ServersStatusDTO[]
+    panicErrorsUsers: string
+  }) {
+    if (notGetData.length > 0) {
+      await this.discord.sendMessage({
+        field: {
+          name: `Atenção ${panicErrorsUsers}`,
+          value:
+            'Não foi possível recuperar os dados das seguintes URLs'
+        },
+        title: 'Urgência!',
+        type: DiscordWebhookPayloadType.Danger
+      })
+
+      await this.sendDiscordMessages({
+        serversStatus: notGetData,
+        title: 'Servidores Sem Retorno',
+        type: DiscordWebhookPayloadType.Danger
+      })
+    }
+  }
+
+  private async sendServersWarningMessage({
+    panicErrorsUsers,
+    warning
+  }: {
+    panicErrorsUsers: string
+    warning: ServersStatusDTO[]
+  }) {
+    if (warning.length > 0) {
+      await this.discord.sendMessage({
+        field: {
+          name: `Atenção ${panicErrorsUsers}`,
+          value:
+            'Os seguintes servidores excederam a margem de uso recomendada'
+        },
+        title: 'Alerta!',
+        type: DiscordWebhookPayloadType.Alert
+      })
+
+      await this.sendDiscordMessages({
+        serversStatus: warning,
+        title: 'Acima do Limite Recomendado',
+        type: DiscordWebhookPayloadType.Alert
+      })
+    }
+  }
   }
 }
